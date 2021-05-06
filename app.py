0ef0cfb0c43c8b4ce833a636e3c80bc21c75c59d
Athora190 <jademarte7@gmail.com>
2021-05-06 18:41:13 -0400

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import flask
+import os
+import pickle
+import pandas as pd
+from skimage import io
+from skimage import transform
 
+app = flask.Flask(__name__,template_folder='templates')
+
+path_to_text_classifier = ''
+
+with open(path_to_text_classifier, 'rb') as f:
+    model = pickle.load(f)
+
+
+@app.route('/', methods=['GET','POST'])
+def main():
+    
