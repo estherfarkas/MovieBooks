0332742255614723912b9c2c812793bf3494c9ea
Athora190 <jademarte7@gmail.com>
2021-05-06 19:13:08 -0400

    adding text and vectorizer

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from skimage import transform
 
 app = flask.Flask(__name__,template_folder='templates')
 
-path_to_text_classifier = ''
-path_to_vectorizer = ''
+path_to_text_classifier = 'models/text_classifier.pkl'
+path_to_vectorizer = 'models/vectorizer.pkl'
 
 with open(path_to_text_classifier, 'rb') as f:
     model = pickle.load(f)
