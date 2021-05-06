7d07be63a642f2da16bed9b8a6edde2c1ea593f2
Athora190 <jademarte7@gmail.com>
2021-05-06 19:03:24 -0400

    app.py

--- app.py
+++ app.py
@@ -8,11 +8,32 @@ from skimage import transform
 app = flask.Flask(__name__,template_folder='templates')
 
 path_to_text_classifier = ''
+path_to_vectorizer = ''
 
 with open(path_to_text_classifier, 'rb') as f:
     model = pickle.load(f)
 
+with open(path_to_vectorizer,'rb') as f:
+    vectorizer = pickle.load(f)
 
 @app.route('/', methods=['GET','POST'])
 def main():
-    
+   if flask.request.method == 'GET':
+       return(flask.render_template('index.html'))
+    if flask.request.method == 'POST':
+        user_input_text = flask.request.form['input'] 
+        X = vectorizer.transform([user_input_text])
+        predictions = model.predict(X)
+        prediction = predictions[0]
+        predicted_probas = model.predict_proba(X)
+        predicted_proba = predicted_probas[0]
+        precent_democrat = predicted_proba[0]
+        precent_republican = predicted_proba[1]
+        return flask.render_temple('index.html', 
+            input_text=user_input_text,
+            result = prediction, 
+            precent_democrat=precent_democrat,
+            precent_republican=precent_republican)
+
+if __name__ =='__main__':
+    app.run(debug=True)
